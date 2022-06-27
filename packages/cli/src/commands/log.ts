b0590c0b4d39358c1594979625e4ec5a913f831c
City <city@nanzc.com>
2022-06-27 13:05:36 +0800

    feat(@lough/git-cli): replace 换行符

--- packages/cli/src/commands/log.ts
+++ packages/cli/src/commands/log.ts
@@ -5,7 +5,9 @@ const action = async () => {
   const { stdout } = execa.commandSync(`git log --date=iso --format=${JSON.stringify(GIT_LOG_FORMAT_FIELD)},`);
 
   const log: Record<keyof typeof GIT_LOG_FORMAT_FIELD, string> = JSON.parse(
-    `[${stdout.substring(0, stdout.length - 1)}]`
+    `[${stdout
+      .substring(0, stdout.length - 1)
+      .replace(/"[^"]*":"[^"]*(\n)[^"]*"/g, ($0, $1) => $0.replace(/\n"/g, '\\n"'))}]`
   );
   console.log(log);
 };
