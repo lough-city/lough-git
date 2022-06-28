7a987b8b0896262c55b339d378c9e6df4b5588c0
City <city@nanzc.com>
2022-06-28 23:53:10 +0800

    feat(@lough/git-cli): 梳理逻辑

--- packages/cli/src/commands/log.ts
+++ packages/cli/src/commands/log.ts
@@ -1,12 +1,29 @@
 import execa from 'execa';
+import fs from 'fs';
+import lough from '../config/lough';
 import { GIT_LOG_FORMAT_FIELD } from '../constants/log';
+import { PACKAGE_MANAGE_TOOL } from '../constants/npm';
 import { GitLog } from '../typings/log';
+import { getPackageManageTool } from '../utils/npm';
 
 /**
  * 读取 package.json 获取生成所需配置
+ *
+ * lerna 多包 逻辑
+ * 无需区分包路径，lerna 多包也需要分别在各个 package 里面 加入命令 lough-git log
+ * 所以直接通过 process.cwd() package.json 拿到相关数据 并且找到属于当前包的 commit log
+ * 非 lerna 包无需判断是否是当前包的提交
  */
 
 const action = async () => {
+  console.log(111111, process.cwd(), __dirname);
+  return;
+
+  const packageManageTool = getPackageManageTool();
+
+  lough.packageManageTool = packageManageTool as PACKAGE_MANAGE_TOOL;
+  if (fs.existsSync(`${process.cwd()}/lerna.json`)) lough.isMorePackage = true;
+
   const { stdout } = execa.commandSync(`git log --date=iso --format=${JSON.stringify(GIT_LOG_FORMAT_FIELD)},`);
 
   const logList: Array<GitLog> = JSON.parse(
